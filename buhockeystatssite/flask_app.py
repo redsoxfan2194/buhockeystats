257934679f6227dbad7a5f599e256bd7792441f2
jesse <jvbelang@gmail.com>
2023-05-28 18:30:34 -0400

    Refactor Format Results
    - Extracts code related to formatting the table into its own file
    - Refactors methods for coloring the table rows
    - Refactors other logic

--- buhockeystatssite/flask_app.py
+++ buhockeystatssite/flask_app.py
@@ -1,4 +1,5 @@
 from flask import Flask,render_template,request,jsonify
+from formatresults import format_results
 from burecordbook import *
 
 print('Generating...')
@@ -244,7 +245,30 @@ def records():
         result+=str(res)+'-'
     result=result.rstrip('-')
     if request.method == 'GET':
-        return render_template('records.html',result=result,query='',resTable=formatResults(dfRes),opponents_values=getOpponentList(dfOrig),season_values=list(dfOrig.season.unique()),arena_values=sorted(list(dfOrig.arena.unique())),buscore=buscore,oppscore=oppscore,isAscending=True,selected_sort='date',startYear=dfOrig.year.min(),endYear=dfOrig.year.max(),minYear=minYear,maxYear=maxYear,selected_startSeas=dfOrig.season.min(),selected_endSeas=dfOrig.season.max(),selected_range='season',hideExStatus="true",tourney_values=getTourneyList(dfOrig),coach_values=list(dfOrig.coach.unique()),selected_day=0)
+        return render_template(
+            'records.html',
+            result=result,
+            query='',
+            resTable=format_results(dfRes),
+            opponents_values=getOpponentList(dfOrig),
+            season_values=list(dfOrig.season.unique()),
+            arena_values=sorted(list(dfOrig.arena.unique())),
+            buscore=buscore,
+            oppscore=oppscore,
+            isAscending=True,
+            selected_sort='date',
+            startYear=dfOrig.year.min(),
+            endYear=dfOrig.year.max(),
+            minYear=minYear,
+            maxYear=maxYear,
+            selected_startSeas=dfOrig.season.min(),
+            selected_endSeas=dfOrig.season.max(),
+            selected_range='season',
+            hideExStatus="true",
+            tourney_values=getTourneyList(dfOrig),
+            coach_values=list(dfOrig.coach.unique()),
+            selected_day=0
+        )
     if request.method == 'POST':
         form_data = request.form
         if(form_data['gender']=='Mens'):
@@ -342,7 +366,16 @@ def records():
         else:
           hideEx=""
         dfRes=dfRes.sort_values(form_data['sortval'],ascending=sortType)
-        return jsonify(resTable=formatResults(dfRes),result=result,opponents_values=getOpponentList(dfOrig),season_values=list(dfOrig.season.unique()),arena_values=sorted(list(dfOrig.arena.unique())),tourney_values=getTourneyList(dfOrig),coach_values=list(dfOrig.coach.unique()),minYear=int(minYear))
+        return jsonify(
+            resTable=format_results(dfRes),
+            result=result,
+            opponents_values=getOpponentList(dfOrig),
+            season_values=list(dfOrig.season.unique()),
+            arena_values=sorted(list(dfOrig.arena.unique())),
+            tourney_values=getTourneyList(dfOrig),
+            coach_values=list(dfOrig.coach.unique()),
+            minYear=int(minYear)
+        )
  
 def convertToHtmlTable(input_string):
     rows = input_string  
@@ -384,31 +417,7 @@ def convertToHtmlTable(input_string):
     html_table += '\n</tbody>\n</table>'
     return html_table
 
-def formatResults(dfRes):
-    dfRes=dfRes[['date','opponent','result','scoreline','ot','arena','tourney','note']].copy()
-    dfRes.loc[dfRes['tourney'].isnull(),'tourney']=''
-    dfRes.loc[dfRes['ot'].notnull(),'ot']=" (" + dfRes.loc[dfRes['ot'].notnull(),'ot'] + ")"
-    dfRes.loc[dfRes['ot'].isnull(),'ot']=''
-    dfRes['scoreline']+=dfRes['ot']
-    notes=list(dfRes['note'].unique())
-    if(len(notes)>1 or ((len(notes)==1) and notes[0]!='')):
-      dfRes=dfRes[['date','opponent','result','scoreline','arena','tourney','note']]
-    else:
-      dfRes=dfRes[['date','opponent','result','scoreline','arena','tourney']]
-    dfRes['date']=dfRes['date'].dt.strftime('%m/%d/%Y')
-    dfRes.rename(columns={'date': 'Date', 'opponent': 'Opponent', 'result': 'Result', 'scoreline':'Score', 'arena':'Location', 'note':'Note', 'tourney':'Tournament'}, inplace = True)
-    style = dfRes.style.apply(colorwinner,axis=1).hide(axis='index')
-    headers = {
-        'selector': 'th:not(.index_name)',
-        'props': 'color: white;'
-    }
-    table={
-        'selector': 'table',
-        'props': [('class', 'sortable')]
-    }
-    style.set_table_styles([headers,table])
-    dfRes=style.to_html(index_names=False,render_links=True)
-    return dfRes
+
 def formatStats(dfRes):
     dfRes=dfRes.copy()
     if('career' in dfRes.columns and 'pts' in dfRes.columns):
@@ -451,103 +460,7 @@ def formatStats(dfRes):
     style.set_table_styles([headers,table])
     dfRes=style.to_html(index_names=False,render_links=True).replace(">nan<",">-<").replace('<th','<th onclick=setSort(this)')
     return dfRes
-    
-def colorwinner(row):
-    teamColors={"Yale":("#0A2240","#FFFFFF"),
-      "Harvard":("#A31F36","#FFFFFF"),
-      "Princeton":("#000000","#FF6000"),
-      "Army":("#2C2A29","#D3BC8D"),
-      "Dartmouth":("#00693E","#FFFFFF"),
-      "Cornell":("#B31B1B","#FFFFFF"),
-      "Rensselaer":("#E2231B","#FFFFFF"),
-      "Michigan Tech":("#000000","#FFCD00"),
-      "Minnesota":("#5B0013","#FFB71E"),
-      "Clarkson":("#03522B","#FFD204"),
-      "Michigan":("#00274C","#FFCB05"),
-      "Boston College":("#8C2232","#DBCCA6"),
-      "St. Thomas":("#500878","#97999B"),
-      "New Hampshire":("#041E42","#BBBCBC"),
-      "Brown":("#4E3629","#C00404"),
-      "Boston University":("#CC0000","#FFFFFF"),
-      "Colgate":("#821019","#FFFFFF"),
-      "Massachusetts":("#881C1C","#FFFFFF"),
-      "Northeastern":("#000000","#D41B2C"), #flipped
-      "St. Cloud State":("#A10209","#FFFFFF"),
-      "St. Lawrence":("#AF1E2D","#4B2B23"),
-      "Colorado College":("#000000","#EFAB1E"),
-      "Union":("#822433","#FFFFFF"),
-      "Michigan State":("#173F35","#FFFFFF"),
-      "North Dakota":("#009A44","#FFFFFF"),
-      "Minnesota Duluth":("#7A0019","#FFCC33"),
-      "American International":("#000000","#FFB60F"),
-      "Wisconsin":("#C5050C","#FFFFFF"),
-      "Denver":("#BA0C2F","#A89968"),
-      "Alaska":("#1E59AE","#FCD006"),
-      "Providence":("#000000","#FFFFFF"),
-      "Bemidji State":("#004D44","#FFFFFF"),
-      "Merrimack":("#003768","#F1C400"),
-      "Notre Dame":("#0C2340","#C99700"),
-      "Connecticut":("#000E2F","#FFFFFF"),
-      "Ohio State":("#BA0C2F","#A1B1B7"),
-      "Vermont":("#005710","#FFC20E"),
-      "RIT":("#F76902","#FFFFFF"),
-      "Holy Cross":("#602D89","#FFFFFF"),
-      "Lake Superior State":("#003F87","#FFC61E"),
-      "UMass Lowell":("#0067B1","#C8102E"),
-      "Air Force":("#003594","#B2B4B2"),
-      "Bowling Green":("#4F2C1D","#FF7300"),
-      "Minnesota State":("#480059","#F7E400"),
-      "Western Michigan":("#532E1F","#F1C500"),
-      "Maine":("#003263","#B0D7FF"),
-      "Ferris State":("#BA0C2F","#FFD043"),
-      "Quinnipiac":("#0A2240","#FFB819"),
-      "Northern Michigan":("#095339","#FFC425"),
-      "Bentley":("#1B5FAA","#FFFFFF"),
-      "Miami":("#B61E2E","#FFFFFF"),
-      "Stonehill":("#2F2975","#FFFFFF"),
-      "Canisius":("#0E2756","#FFBA00"),
-      "Alaska Anchorage":("#00583D","#FFC425"),
-      "Mercyhurst":("#0F4F44","#FFFFFF"),
-      "Sacred Heart":("#CD1041","#FFFFFF"),
-      "Niagara":("#582C83","#FFFFFF"),
-      "Nebraska Omaha":("#000000","#D71920"),
-      "Penn State":("#001E44","#FFFFFF"),
-      "Robert Morris":("#001E41","#AA182C"),
-      "Arizona State":("#7D2248","#FFC72C"),
-      "Long Island":("#69B3E7","#FFC72C"),
-      "Lindenwood":("#000000","#B5A36A"),
-      "Augustana":("#002D62","#FFDD00"),
-      "Tufts":("#3E8EDE","#512C1D"),
-      "MIT":("#231F20","#B20D35"),
-      "Toronto":("#002A5C","#FFFFFF"),
-      "Colby":("#254893","#FFFFFF"),
-      "Dalhousie":("#242424","#FFD400"),
-      "Pennsylvania":("#990000","#011F5B"),
-      "St. Louis": ("#003DA5","#C8C9C7"),
-      "Norwich":("#9C0A2E","#B99457"),
-      "Queen's":("#B90E31","#FABD0F"),
-      "Middlebury":("#37538C","#FFFFFF"),
-      "Williams":("#362061","#FECC06"),
-      "Hamilton College":("#002F86","#D6BA8B"),
-      "St. Francis Xavier":("#0C2340","#FFFFFF"),
-      "Syracuse":("#F76900","#FFFFFF"),
-      "Wayne State":("#0C5449","#FFCC33")}
-    if(row['Opponent'] not in teamColors.keys()):
-        oppBGColor='white'
-        oppColor='black'
-    else:
-        oppBGColor=teamColors[row['Opponent']][0]
-        oppColor=teamColors[row['Opponent']][1]
-    if row['Result'] == "W":
-        return ['background-color: #cc0000; color:white;text-align:center'] * len(row)
-    
-    elif row['Result'] == "L":
-        return [f'background-color: {oppBGColor}; color:{oppColor};text-align:center'] * len(row)
 
-    elif row['Result'] == "T":
-        return [f'background-color: grey;text-align:center'] * len(row)
-    else:
-        return ['background-color: black; color:white;  text-align:center'] * len(row)
    
 
     
---
+++ buhockeystatssite/formatresults.py
@@ -0,0 +1,148 @@
+COLUMNS = {
+    'date': 'Date',
+    'opponent': 'Opponent',
+    'result': 'Result',
+    'scoreline': 'Score',
+    'arena': 'Location',
+    'tourney': 'Tournament',
+    'note': 'Note'
+}
+TABLE_STYLES = [
+    {
+        'selector': 'th:not(.index_name)',
+        'props': 'color: #FFFFFF;'
+    }, {
+        'selector': 'table',
+        'props': [('class', 'sortable')]
+    }
+]
+
+BU_COLOR = "#FFFFFF"
+BU_BG_COLOR = '#CC0000'
+
+DEFAULT_COLOR = '#000000'
+DEFAULT_BG_COLOR = '#FFFFFF'
+
+TIE_COLOR = '#808080'
+TIE_BG_COLOR = '#000000'
+
+TEAM_COLORS = {
+    "Yale": ("#0A2240", "#FFFFFF"),
+    "Harvard": ("#A31F36", "#FFFFFF"),
+    "Princeton": ("#000000", "#FF6000"),
+    "Army": ("#2C2A29", "#D3BC8D"),
+    "Dartmouth": ("#00693E", "#FFFFFF"),
+    "Cornell": ("#B31B1B", "#FFFFFF"),
+    "Rensselaer": ("#E2231B", "#FFFFFF"),
+    "Michigan Tech": ("#000000", "#FFCD00"),
+    "Minnesota": ("#5B0013", "#FFB71E"),
+    "Clarkson": ("#03522B", "#FFD204"),
+    "Michigan": ("#00274C", "#FFCB05"),
+    "Boston College": ("#8C2232", "#DBCCA6"),
+    "St. Thomas": ("#500878", "#97999B"),
+    "New Hampshire": ("#041E42", "#BBBCBC"),
+    "Brown": ("#4E3629", "#C00404"),
+    "Boston University": ("#CC0000", "#FFFFFF"),
+    "Colgate": ("#821019", "#FFFFFF"),
+    "Massachusetts": ("#881C1C", "#FFFFFF"),
+    "Northeastern": ("#000000", "#D41B2C"),  # flipped
+    "St. Cloud State": ("#A10209", "#FFFFFF"),
+    "St. Lawrence": ("#AF1E2D", "#4B2B23"),
+    "Colorado College": ("#000000", "#EFAB1E"),
+    "Union": ("#822433", "#FFFFFF"),
+    "Michigan State": ("#173F35", "#FFFFFF"),
+    "North Dakota": ("#009A44", "#FFFFFF"),
+    "Minnesota Duluth": ("#7A0019", "#FFCC33"),
+    "American International": ("#000000", "#FFB60F"),
+    "Wisconsin": ("#C5050C", "#FFFFFF"),
+    "Denver": ("#BA0C2F", "#A89968"),
+    "Alaska": ("#1E59AE", "#FCD006"),
+    "Providence": ("#000000", "#FFFFFF"),
+    "Bemidji State": ("#004D44", "#FFFFFF"),
+    "Merrimack": ("#003768", "#F1C400"),
+    "Notre Dame": ("#0C2340", "#C99700"),
+    "Connecticut": ("#000E2F", "#FFFFFF"),
+    "Ohio State": ("#BA0C2F", "#A1B1B7"),
+    "Vermont": ("#005710", "#FFC20E"),
+    "RIT": ("#F76902", "#FFFFFF"),
+    "Holy Cross": ("#602D89", "#FFFFFF"),
+    "Lake Superior State": ("#003F87", "#FFC61E"),
+    "UMass Lowell": ("#0067B1", "#C8102E"),
+    "Air Force": ("#003594", "#B2B4B2"),
+    "Bowling Green": ("#4F2C1D", "#FF7300"),
+    "Minnesota State": ("#480059", "#F7E400"),
+    "Western Michigan": ("#532E1F", "#F1C500"),
+    "Maine": ("#003263", "#B0D7FF"),
+    "Ferris State": ("#BA0C2F", "#FFD043"),
+    "Quinnipiac": ("#0A2240", "#FFB819"),
+    "Northern Michigan": ("#095339", "#FFC425"),
+    "Bentley": ("#1B5FAA", "#FFFFFF"),
+    "Miami": ("#B61E2E", "#FFFFFF"),
+    "Stonehill": ("#2F2975", "#FFFFFF"),
+    "Canisius": ("#0E2756", "#FFBA00"),
+    "Alaska Anchorage": ("#00583D", "#FFC425"),
+    "Mercyhurst": ("#0F4F44", "#FFFFFF"),
+    "Sacred Heart": ("#CD1041", "#FFFFFF"),
+    "Niagara": ("#582C83", "#FFFFFF"),
+    "Nebraska Omaha": ("#000000", "#D71920"),
+    "Penn State": ("#001E44", "#FFFFFF"),
+    "Robert Morris": ("#001E41", "#AA182C"),
+    "Arizona State": ("#7D2248", "#FFC72C"),
+    "Long Island": ("#69B3E7", "#FFC72C"),
+    "Lindenwood": ("#000000", "#B5A36A"),
+    "Augustana": ("#002D62", "#FFDD00"),
+    "Tufts": ("#3E8EDE", "#512C1D"),
+    "MIT": ("#231F20", "#B20D35"),
+    "Toronto": ("#002A5C", "#FFFFFF"),
+    "Colby": ("#254893", "#FFFFFF"),
+    "Dalhousie": ("#242424", "#FFD400"),
+    "Pennsylvania": ("#990000", "#011F5B"),
+    "St. Louis": ("#003DA5", "#C8C9C7"),
+    "Norwich": ("#9C0A2E", "#B99457"),
+    "Queen's": ("#B90E31", "#FABD0F"),
+    "Middlebury": ("#37538C", "#FFFFFF"),
+    "Williams": ("#362061", "#FECC06"),
+    "Hamilton College": ("#002F86", "#D6BA8B"),
+    "St. Francis Xavier": ("#0C2340", "#FFFFFF"),
+    "Syracuse": ("#F76900", "#FFFFFF"),
+    "Wayne State": ("#0C5449", "#FFCC33")
+}
+
+
+def format_results(df_results):
+    table_data = df_results[['date', 'opponent', 'result', 'scoreline', 'ot', 'arena', 'tourney', 'note']].copy()
+    table_data.fillna('', inplace=True)
+
+    table_data['date'] = table_data['date'].dt.strftime('%m/%d/%Y')
+
+    table_data.loc[table_data['ot'].notnull(), 'scoreline'] = table_data['scoreline'] + " (" + table_data.loc[table_data['ot'].notnull(), 'ot'] + ")"
+    table_data.drop(['ot'], axis=1, inplace=True)
+
+    if len(list(table_data['note'].unique())) < 1:
+        table_data.drop(['note'], axis=1, inplace=True)
+
+    table_data.rename(columns=COLUMNS, inplace=True)
+    return table_data.style.apply(style_row, axis=1).hide(axis='index').set_table_styles(TABLE_STYLES).to_html(index_names=False, render_links=True)
+
+
+def style_row(row):
+    background, font = winner_colors(row['Result'], row['Opponent'])
+    return [f'background-color: {background}; color: {font}; text-align:center'] * len(row)
+
+
+def winner_colors(result, opponent):
+    if result == "W":
+        return BU_BG_COLOR, BU_COLOR
+    elif result == "L":
+        return opponent_colors(opponent)
+    elif result == "T":
+        return TIE_BG_COLOR, TIE_COLOR
+    else:
+        return DEFAULT_BG_COLOR, DEFAULT_COLOR
+
+
+def opponent_colors(opponent):
+    if opponent not in TEAM_COLORS.keys():
+        return DEFAULT_BG_COLOR, DEFAULT_COLOR
+    else:
+        return TEAM_COLORS[opponent][0], TEAM_COLORS[opponent][1]
