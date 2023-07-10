1ded6ddcc2b6332b8537bec0f51121c096bc354a
ch_scorebot <44101012+redsoxfan2194@users.noreply.github.com>
2023-07-10 19:40:51 -0400

    change image on hover

--- buhockeystatssite/static/js/home.js
+++ buhockeystatssite/static/js/home.js
@@ -9,10 +9,33 @@ event.preventDefault();
         data: formData,
         success: function (response) {
         document.getElementById("result").textContent = response.result;
-        console.log(response.result);
         },
         error: function (xhr, status, error) {
             console.error("Error:", error);
         },
     });
+}
+
+function changeStatsBotImage() {
+  document.getElementById('stats-bot-logo').src = '../static/images/statsbotlogo_hover.ico';
+}
+
+function restoreStatsBotImage() {
+  document.getElementById('stats-bot-logo').src = '../static/images/statsbotlogo.ico';
+}
+
+function changeRecordsImage() {
+  document.getElementById('records-logo').src = '../static/images/recordslogo_hover.ico';
+}
+
+function restoreRecordsImage() {
+  document.getElementById('records-logo').src = '../static/images/recordslogo.ico';
+}
+
+function changePlayersImage() {
+  document.getElementById('players-logo').src = '../static/images/jersey_home.ico';
+}
+
+function restorePlayersImage() {
+  document.getElementById('players-logo').src = '../static/images/jersey_road.ico';
 }
