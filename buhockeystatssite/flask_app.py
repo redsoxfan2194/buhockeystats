9950711c1785b060d90caf2694ecd57f716c162c
ch_scorebot <44101012+redsoxfan2194@users.noreply.github.com>
2023-07-29 08:21:37 -0400

    add 404 page

--- buhockeystatssite/flask_app.py
+++ buhockeystatssite/flask_app.py
@@ -25,6 +25,10 @@ print('Record Book Initialized')
 
 app = Flask(__name__)
 
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template('404.html'), 404
 
 @app.route('/', methods=['POST', 'GET'])
 def homepage():
