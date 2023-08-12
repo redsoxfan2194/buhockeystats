e7c0d5510f010622ec03bec9d98a19ef25bb85a4
ch_scorebot <44101012+redsoxfan2194@users.noreply.github.com>
2023-08-12 07:23:45 -0400

    put stats bot results on card

--- buhockeystatssite/static/js/statsbot.js
+++ buhockeystatssite/static/js/statsbot.js
@@ -1,1 +1,10 @@
 console.log("loading statsbot.js");
+
+document.addEventListener("DOMContentLoaded", function(event) {
+if(document.getElementById('card-query').textContent != ""){
+  document.getElementById('result-card').style.display  = "block";
+}
+else{
+  document.getElementById('result-card').style.display ="none";
+}
+});
