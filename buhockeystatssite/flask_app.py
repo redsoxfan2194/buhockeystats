b6ee4b486b543d0e621307fcada9f99a97bb1c18
ch_scorebot <44101012+redsoxfan2194@users.noreply.github.com>
2023-08-16 20:07:28 -0400

    fix path in sitemap

--- buhockeystatssite/flask_app.py
+++ buhockeystatssite/flask_app.py
@@ -24,7 +24,7 @@ app = Flask(__name__)
 
 @app.route('/sitemap.xml', methods=['GET'])
 def generate_sitemap():
-    pages = ['/', '/about', '/players', '/statsbot', '/records', '/trivia', '/triviagame']
+    pages = ['', 'about', 'players', 'statsbot', 'records', 'trivia', 'triviagame']
 
     xml_sitemap = '<?xml version="1.0" encoding="UTF-8"?>\n'
     xml_sitemap += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n'
